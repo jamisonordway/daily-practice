262f7f66c67958200135cb81e6557a8d1e2d3bf6
JamisonOrdway <jamisonordway@gmail.com>
2018-10-01 13:32:25 -0600

    Completes palindrome exercise

---
+++ javascript_3.js
@@ -0,0 +1,21 @@
+// palindromes!
+
+// function isPalindrome(str) {
+//   const revString = str.split('').reverse().join('');
+
+//   return revString === str;
+// }
+
+// The above could be refactored.
+
+function isPalindrome(str) {
+  str = str.toLowerCase();
+  return str === str.split('').reverse().join('');
+}
+
+// this will return the string (split on each char into an array and reversed, then joined together again)
+// if it is the same as str (the original passed in value)
+// Make sure to account for capitalization!
+
+isPalindrome('racecar');
+isPalindrome('sup');
