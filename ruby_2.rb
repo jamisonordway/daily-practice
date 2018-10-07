7fea720ca571f2df31096ab7029aa57487331b77
JamisonOrdway <jamisonordway@gmail.com>
2018-10-07 10:57:36 -0600

    starts over bracket matcher

--- ruby_2.rb
+++ ruby_2.rb
@@ -9,37 +9,3 @@
 # bracket('{[)][]}');
 # // => false
 
-def brackets(values)
-  values.collect do |string|
-    balanced?(string: string)
-  end
-end
-
-def balanced?(string)
-  array = string.split("")
-  false if array.length.odd?
-  openers = []
-  until array.empty?
-    if is_opener?(first: array.first)
-      openers.unshift(array.shift)
-    else
-      false unless correct_closer?(closer: array.shift, need_to_close: openers.shift)
-    end
-  end
-  true
-end
-
-def is_opener?(first)
-  ["(", "{", "["].any? { |opener| opener == first }
-end
-
-def correct_closer?(closer: , need_to_close: )
-  closer == needed_closer = case need_to_close
-  when "{"
-    "}"
-  when "["
-    "]"
-  when "("
-    ")"
-  end
-end
