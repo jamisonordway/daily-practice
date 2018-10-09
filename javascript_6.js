c42b00cb6de6d74e0e3f84ba2c26a9e42a95c3ef
JamisonOrdway <jamisonordway@gmail.com>
2018-10-08 18:29:29 -0600

    completes better solution for fibonacci sequence exercise

--- javascript_6.js
+++ javascript_6.js
@@ -3,3 +3,14 @@
 // and each subsequent number is the sum of the previous two. 
 // As an example, here are the first 10 Fibonnaci numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, and 34.
 
+const fibonacci_sequence = function (n) {
+  if (n === 1) {
+    return [0, 1]
+  } else {
+    let sequence = fibonacci_sequence(n - 1);
+    sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+    return sequence;
+  }
+};
+
+console.log(fibonacci_sequence(100))
