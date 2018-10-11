6b17255592a1a5b989b277a217c5ab6ab57f6094
JamisonOrdway <jamisonordway@gmail.com>
2018-10-11 12:22:06 -0600

    Cleans up extra lines (classes)

--- ruby_1.rb
+++ ruby_1.rb
@@ -1,6 +1,5 @@
 # Write a function that will loop through a list of integers and 
 # print the index of each element after a 3 second delay.
-class Index
 
   def self.print_stuff(array)
     array.each_with_index do |array, index|
@@ -11,4 +10,3 @@ class Index
 
   given = [1, 2, 3, 5, 23]
   print_stuff(given)
-end
