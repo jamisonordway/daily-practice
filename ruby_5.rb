5a2a558d91202130531f0b7fc4792da3acd63abe
JamisonOrdway <jamisonordway@gmail.com>
2018-10-24 11:19:09 -0600

    completes first duplicate

--- ruby_5.rb
+++ ruby_5.rb
@@ -1,20 +1,13 @@
 a = [2, 1, 3, 5, 3, 2]
 
-# def first_duplicate(a)
-#   a.each_with_object(Hash.new(0)) do |val, hash|
-#     hash[val] += 1
-#   end.each_with_object([]) do |(val, count), result|
-#     if count > 1
-#       result << val
-#     end
-#   end
-# end
 
 def first_duplicate(a)
-  a.map do |char|
-    [char, a.count(char)]
-  end.drop_while do |i|
-    i[1] < 1
-  end.first[0]
+  counts = []
+  a.each do |v|
+    return v if counts[v]
+    counts[v] = true
+  end
+  -1
 end
+
 puts first_duplicate(a)
